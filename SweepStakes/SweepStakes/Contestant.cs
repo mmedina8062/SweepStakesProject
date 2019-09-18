9b33d26713899795aa2df57b89bc0a03d60c4758
unknown <mmedina8062@gmail.com>
2019-09-18 16:03:06 -0500

    changed my constestant interface back to class

---
+++ SweepStakes/SweepStakes/Contestant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    public abstract class Contestant
+    {
+        //member variables
+        public Contestant contestant;
+
+        //ctor
+        public Contestant()
+        {
+
+        }
+
+        //member methods
+
+
+    }
+}
